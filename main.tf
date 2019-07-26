b1660c9a2a9e5102e563b7d8df7e5ee7179b4fea
Andrei Popa <andrei@hashicorp.com>
2019-07-26 23:03:48 +0200

    add TF code for random server name

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "popa-org"
+
+    workspaces {
+      name = "server-name"
+    }
+  }
+}
+
+resource "random_pet" "server" {}
+
+output "server_name" {
+  value       = "${random_pet.server.id}"
+  description = "Random pet resource to name server infrastructure"
+}
